c1285c79676daa1d853b675162cc00681290184f
ldicandia <ldicandia@itba.edu.ar>
2025-10-13 10:57:29 -0300

    feat: lexical-analysis

--- src/main/c/frontend/lexical-analysis/FlexActions.c
+++ src/main/c/frontend/lexical-analysis/FlexActions.c
@@ -160,3 +160,11 @@ CompilationStatus UnknownLexemeAction() {
   destroyToken(token);
   return FAILED;
 }
+
+CompilationStatus TokenLexemeAction(TokenLabel label) {
+  Token *token = createToken(_lexicalAnalyzer, label);
+  _logTokenAction(__FUNCTION__, token);
+  CompilationStatus status = pushToken(_lexicalAnalyzer, token);
+  destroyToken(token);
+  return status;
+}
